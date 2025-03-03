eeb76e748ee0354fc5ce57ee08e60c80e7d208aa
Bendi11 <bkliebmann@gmail.com>
2025-03-03 14:17:58 -0800

    Added build config for STM32F405

---
+++ flash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+TARGET_DIR="./target/thumbv7em-none-eabihf/release"
+ELF_NAME="bingo-fc"
+HEX_NAME="$ELF_NAME.hex"
+DFU_NAME="$ELF_NAME.dfu"
+
+arm-none-eabi-objcopy -O ihex "$TARGET_DIR/$ELF_NAME" "$TARGET_DIR/$HEX_NAME"
+hex2dfu -i "$TARGET_DIR/$HEX_NAME" -o "$TARGET_DIR/$DFU_NAME"
+dfu-util -D "$TARGET_DIR/$DFU_NAME"
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use stm32f4xx_hal::{pac, gpio::GpioExt};
+
+extern crate panic_halt;
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+
+    loop {
+        core::hint::spin_loop()
+    }
+}
